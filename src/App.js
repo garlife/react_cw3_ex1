e2899dc8d7e92a1492bba2f923b3cb0e2f4871a6
garlife <garlife@gmail.com>
2021-05-09 14:19:04 +0300

    practice done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Column, Table, Cell } from "@blueprintjs/table";
 
 function App() {
+  const dataSource = {
+    headers: {
+      artists: "Исполнители",
+      album: "Альбомы",
+      song: "Песны",
+    },
+    rows: [
+      {
+        artists: "Queen",
+        album: "Innuendo",
+        song: "Innuendo",
+      },
+      {
+        artists: "AWOLNATION",
+        album: "Run",
+        song: "Run",
+      },
+      {
+        artists: "Kanye West",
+        album: "Yeezus",
+        song: "Black Skinhead",
+      },
+    ],
+  };
+
+  const cellRenderer = (name) => (rowIndex) => {
+    return <Cell>{dataSource.rows[rowIndex][name]}</Cell>;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table numRows={dataSource.rows.length}>
+        <Column
+          name={dataSource.headers.artists}
+          cellRenderer={cellRenderer("artists")}
+        />
+        <Column
+          name={dataSource.headers.album}
+          cellRenderer={cellRenderer("album")}
+        />
+        <Column
+          name={dataSource.headers.song}
+          cellRenderer={cellRenderer("song")}
+        />
+      </Table>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "normalize.css";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+import "@blueprintjs/datetime/lib/css/blueprint-datetime.css";
+import "@blueprintjs/table/lib/css/table.css";
 
 ReactDOM.render(
   <React.StrictMode>
